d975e60f90885a99bda0dcb87ac463c7288a9654
Flayneorange <flayne1996@gmail.com>
2016-05-21 21:08:27 -0400

    fixd

--- Source/Engine/Private/Entity.cpp
+++ Source/Engine/Private/Entity.cpp
@@ -2,8 +2,7 @@
 #include <Game.h>
 #include <OpenGL3Render/Public/Render.h>
 #include <Engine.h>
-#include <sstream>
-#include <string>
+#include <SFMT.h>
 
 C3_NAMESPACE_BEGIN
 //Player
@@ -17,24 +16,22 @@ void FPlayer::Destroy() {
 }
 
 void FPlayer::Update() {
-	const float speed = 18.0f;
-	glm::vec3 move(0.0f);
+	const float Speed = 18;
+	const float SpeedUpOverTime = 0.05;
+	const float Movement =
+		Game.GameTimer.GetDeltaTime() * Speed
+		+ SpeedUpOverTime * Game.GameTimer.GetTotalTime();
 	if (Game.ActiveControls.MoveUp) {
-		move.y += 1.0f;
+		SGEntry->Move(0, Movement, 0);
 	}
 	if (Game.ActiveControls.MoveDown) {
-		move.y -= 1.0f;
+		SGEntry->Move(0, -Movement, 0);
 	}
 	if (Game.ActiveControls.MoveLeft) {
-		move.x -= 1.0f;
+		SGEntry->Move(-Movement, 0, 0);
 	}
 	if (Game.ActiveControls.MoveRight) {
-		move.x += 1.0f;
-	}
-	if (move.x != 0.0f || move.y != 0.0f)
-	{
-		move = glm::normalize(move) * speed * Game.GameTimer.GetDeltaTime();
-		SGEntry->Move(move);
+		SGEntry->Move(Movement, 0, 0);
 	}
 }
 
@@ -42,7 +39,9 @@ void FPlayer::Update() {
 void FEnemy::Init() {
 	SGEntry = RC.Render->NewSGObject();
 	SGEntry->LoadModelFromResource(RC.Engine->BunnyRMesh);
-	SGEntry->Move(20, 0, -20);	//Make it look smaller than the player
+	float XMove = sfmt_genrand_real1(&Game.RandGen) * Game.ScreenSize.x * 2 - Game.ScreenSize.x;
+	float YMove = sfmt_genrand_real1(&Game.RandGen) * Game.ScreenSize.y * 2 - Game.ScreenSize.y;
+	SGEntry->Move(XMove, YMove, -20);
 }
 
 void FEnemy::Destroy() {
@@ -50,12 +49,11 @@ void FEnemy::Destroy() {
 }
 
 void FEnemy::Update() {
-	const float speed = 1;
-	SGEntry->Move(Game.GameTimer.GetDeltaTime() * -speed, 0, 0);
-	FBoundingRect b = SGEntry->GetBoundingRectXY();
-	std::stringstream ss;
-	ss << b.BtmLeft.x << " " << b.TopRight.x << " ";
-	ss << b.BtmLeft.y << " " << b.TopRight.y;
-	FLog::Debug(ss.str().c_str());
+	const float Speed = 3;
+	const float SpeedUpOverTime = 0.05;
+	const float Movement =
+		Game.GameTimer.GetDeltaTime() * Speed
+		+ SpeedUpOverTime * Game.GameTimer.GetTotalTime();
+	SGEntry->Move(0, 0, Movement);
 }
 C3_NAMESPACE_END
